6fed22e5c8c68740d4afeed82955b8d85ae81012
zoker <nguyenthanhtin1994@gmail.com>
2023-10-27 14:08:11 +0700

    part3-done

--- frontend/src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null;
-  }
-
-  return <div className={message.type}>{message.content}</div>;
-};
-
-export default Notification;
